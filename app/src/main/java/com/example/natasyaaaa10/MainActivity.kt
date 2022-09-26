ff08dfb815e5ba73013631b4e05f5e2fed0bae81
Your Name <you@example.com>
2022-09-26 09:14:53 +0700

    first commit

---
+++ app/src/main/java/com/example/natasyaaaa10/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.natasyaaaa10
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button1 = findViewById<Button>(R.id.button1)
+
+
+        button1.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+
+            val button2= findViewById<Button>(R.id.button2)
+
+
+            button2.setOnClickListener {
+                val intent = Intent(this, MainActivity3::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+}
